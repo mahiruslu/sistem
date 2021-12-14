29379c0a9acff14d573e840d831d8ef710ce5f08
Mahir <mhrsl07@gmail.com>
2021-12-14 17:07:16 +0300

    products updated. some styling issues fixed

---
+++ src/Components/Products/ProductCard.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./Products.css";
+
+function ProductCard() {
+    return (
+            <div class="product-card">
+            <div class="product-info">
+              <div className="product-image">
+                <img src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt=""/>
+              </div>
+              <div className="product-title">
+                <h3>Product Name</h3>
+              </div>
+              <div className="product-description">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisi vel consectetur interdum, nisl nisi tincidunt nisi, eget egestas nisl nisi sed nisi. Sed euismod, nisi vel consectetur interdum, nisl nisi tincidunt nisi, eget egestas nisl nisi sed nisi. Sed euismod, nisi vel consectetur interdum, nisl nisi tincidunt nisi, eget egestas nisl nisi sed nisi. Sed euismod, nisi vel consectetur interdum, nisl nisi tincidunt nisi, eget egestas nisl nisi sed nisi. Sed euismod
+                </p>
+              </div>
+                <div className="c">
+                    <p>Ayrıntılı bilgi için bize ulaşın</p>
+                </div>
+          </div>  
+        </div>
+    )
+}
+
+export default ProductCard
--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,30 +1,21 @@
 import React from "react";
 import "./Products.css";
+import ProductCard from "./ProductCard";
 
 function Services() {
+
   return (
-    <div className="products-container">
-      <div className="product-cards">
-        <div class="product-card">
-          <div class="product-info">ürün 1</div>
-        </div>
-        <div class="product-card">
-          <div class="product-info">ürün 2</div>
-        </div>
-        <div class="product-card">
-          <div class="product-info">ürün 3</div>
-        </div>
-        <div class="product-card">
-          <div class="product-info">ürün 4</div>
-        </div>
-        <div class="product-card">
-          <div class="product-info">ürün 5</div>
-        </div>
-        <div class="product-card">
-          <div class="product-info">ürün 6</div>
+
+      <div className="products-container">
+        <div className="product-cards">
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
         </div>
       </div>
-    </div>
+
   );
 }
 
