d265277a6c440c08993ea5a15b016e63c06e3c2b
Mahir Mac <mhrsl07@gmail.com>
2022-01-14 22:31:19 +0300

    home page changed. about section removed.

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import backgroundImage from "../../Images/background.jpg";
-import '../../../node_modules/pure-react-carousel/dist/react-carousel.es.css';
+import "../../../node_modules/pure-react-carousel/dist/react-carousel.es.css";
+import HomeSwiper from "./HomeSwiper";
 
 const styles = {
-  backgroundImage: `url(${backgroundImage})`,
+  // backgroundImage: `url(${backgroundImage})`,
   backgroundPosition: "center",
   backgroundSize: "cover",
   backgroundRepeat: "no-repeat",
-  width: "100%",
+  // width: "100%",
   height: "100vh",
-  display: "flex",
-  flexDirection: "column",
+  // display: "flex",
+  // flexDirection: "column",
   justifyContent: "center",
   alignItems: "center",
 };
@@ -32,11 +33,16 @@ const textStyle = {
 function Home() {
   return (
     <div className="home-container" style={styles}>
-      <div  className="home-inner-container" style={innerStyles}>
-        <p className="home-paragraph" style={textStyle}>
-        Sistem makine kalıp 1999 yılında saç metal kalıp üretimi yaparak faaliyetine başlamıştır. Daha sonra sanayimizin genellikle yurtdışından ithal ettiği ve yurtiçi pazarda eksikliği hissedilen hidrolik ve pnömatik malzemelerin imalatına yönelmiştir.
-        </p>
-      </div>
+      <HomeSwiper />
+
+      {/* //   <div className="home-inner-container" style={innerStyles}>
+    //     <p className="home-paragraph" style={textStyle}>
+    //       Sistem makine kalıp 1999 yılında saç metal kalıp üretimi yaparak
+    //       faaliyetine başlamıştır. Daha sonra sanayimizin genellikle
+    //       yurtdışından ithal ettiği ve yurtiçi pazarda eksikliği hissedilen
+    //       hidrolik ve pnömatik malzemelerin imalatına yönelmiştir.
+    //     </p>
+    //   </div> */}
     </div>
   );
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,69 +1,100 @@
 import React from "react";
-import {Link} from 'react-scroll'
+import { Link } from "react-scroll";
 import logo from "../../Images/shortLogo.png";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFlag,faFlagCheckered,faFlagUsa } from '@fortawesome/free-solid-svg-icons'
-import './Navbar.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFlag,
+  faFlagCheckered,
+  faFlagUsa,
+} from "@fortawesome/free-solid-svg-icons";
+import "./Navbar.css";
 
 function Navbar() {
-  function toggle_handler(){
+  function toggle_handler() {
     var links = document.getElementById("links");
-    var toggle_button  = document.getElementById("toggle_button");
-    links.className === "links" ? links.className = "links_opened" : links.className = "links";
+    var toggle_button = document.getElementById("toggle_button");
+    links.className === "links"
+      ? (links.className = "links_opened")
+      : (links.className = "links");
     console.log(toggle_button.className);
-    toggle_button.className === "toggle_button" ? toggle_button.className = "toggle_button_opened" : toggle_button.className = "toggle_button";
+    toggle_button.className === "toggle_button"
+      ? (toggle_button.className = "toggle_button_opened")
+      : (toggle_button.className = "toggle_button");
   }
 
   return (
     <header className="nav_container">
       <div className="nav_logo">
         <img src={logo} alt="logo" />
-     </div>
-       <div href="#"  className="toggle_button" id="toggle_button" onClick={toggle_handler}>
-          <span className="toggle_line" ></span>
-          <span className="toggle_line" ></span>
-          <span className="toggle_line" ></span>
-       </div>
-     <nav className="links" id="links">
+      </div>
+      <div
+        href="#"
+        className="toggle_button"
+        id="toggle_button"
+        onClick={toggle_handler}
+      >
+        <span className="toggle_line"></span>
+        <span className="toggle_line"></span>
+        <span className="toggle_line"></span>
+      </div>
+      <nav className="links" id="links">
         <ul>
           <li>
-          <Link to="home-wrapper" smooth={true} duration={900} onClick={toggle_handler}>
-              <a className="nav-link" >Anasayfa</a>
-          </Link>
+            <Link
+              to="home-wrapper"
+              smooth={true}
+              duration={900}
+              onClick={toggle_handler}
+            >
+              <a className="nav-link">Anasayfa</a>
+            </Link>
           </li>
-          <li>
+          {/* <li>
           <Link to="about-wrapper" smooth={true} duration={900} className="nav-link" onClick={toggle_handler}>
               <a className="nav-link" >Hakkımızda</a>
           </Link>
-          </li>          
+          </li>           */}
           <li>
-          <Link to="products-wrapper" smooth={true} duration={900} className="nav-link" onClick={toggle_handler}>             
-              <a className="nav-link" >Ürünler</a>
-          </Link>
+            <Link
+              to="products-wrapper"
+              smooth={true}
+              duration={900}
+              className="nav-link"
+              onClick={toggle_handler}
+            >
+              <a className="nav-link">Ürünler</a>
+            </Link>
           </li>
           <li>
-          <Link to="contact-wrapper" smooth={true} duration={900} className="nav-link" onClick={toggle_handler}>              
-              <a className="nav-link" >İletişim</a>
-          </Link>
-          </li>          
-          <li>
+            <Link
+              to="contact-wrapper"
+              smooth={true}
+              duration={900}
+              className="nav-link"
+              onClick={toggle_handler}
+            >
+              <a className="nav-link">İletişim</a>
+            </Link>
+          </li>
+          {/* <li>
             <div className="nav_dropdown">
-                    <FontAwesomeIcon icon={faFlag} />
+              <FontAwesomeIcon icon={faFlag} />
               <div className="nav_dropdown_content">
-                  <span>
-                    <a href="#" className="nav-link" onClick={toggle_handler}>
-                    <FontAwesomeIcon icon={faFlagCheckered} /> Türkçe</a>
-                  </span>
-                  <span>
-                    <a href="#" className="nav-link" onClick={toggle_handler}>
-                      <FontAwesomeIcon icon={faFlagUsa} /> English</a>
-                  </span>
+                <span>
+                  <a href="#" className="nav-link" onClick={toggle_handler}>
+                    <FontAwesomeIcon icon={faFlagCheckered} /> Türkçe
+                  </a>
+                </span>
+                <span>
+                  <a href="#" className="nav-link" onClick={toggle_handler}>
+                    <FontAwesomeIcon icon={faFlagUsa} /> English
+                  </a>
+                </span>
               </div>
             </div>
-          </li>
+          </li> */}
         </ul>
       </nav>
-      
     </header>
   );
 }
